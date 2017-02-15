42ef181e7bdd4466b426a6367e330a662623d8e5
Kevin Siml <appzer@users.noreply.github.com>
2017-02-15 21:44:53 +0100

    Create message_pushsafer.php

---
+++ lang/en/message_pushsafer.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ *
+ * Pushsafer for Moodle is distributed as GPLv3 software, and is provided free of charge without warranty
+ * A full copy of this licence can be found @ http://www.gnu.org/licenses/gpl.html
+ *
+ * @package message_pushsafer
+ * @author Kevin Siml https://www.pushsafer.com
+ * @copyright Copyright &copy; 2016 Kevin Siml pushsafer.com.
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public Licence v3 (See code header for additional terms)
+ */
+
+
+defined('MOODLE_INTERNAL') || die('File cannot be accessed directly.');
+
+$string['pluginname'] = 'Pushsafer';
+$string['privatekey'] = 'Private or Alias Key';
+$string['usertoken'] = 'Device or Device Group ID';
+$string['setprivatekey'] = 'Go to your Pushsafer Dashboard and insert the private or alias key here.';
+$string['notconfigured'] = 'A Pushsafer private or alias key has not been provided.';
+$string['configured'] = 'A Pushsafer private or alias key has been provided.';
