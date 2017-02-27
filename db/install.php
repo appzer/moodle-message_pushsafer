b45ab8ea1c37f8ffbf540e3d9801e27d91ebff2f
Kevin Siml <appzer@users.noreply.github.com>
2017-02-27 12:27:35 +0100

    Add files via upload

--- db/install.php
+++ db/install.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  *
  * Pushsafer for Moodle is distributed as GPLv3 software, and is provided free of charge without warranty
  * A full copy of this licence can be found @ http://www.gnu.org/licenses/gpl.html
  *
- * @package moodle-message_pushsafer
+ * @package message_pushsafer
  * @author Kevin Siml https://www.pushsafer.com
  * @copyright Copyright 2017 Kevin Siml pushsafer.com.
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public Licence v3 (See code header for additional terms)
@@ -22,4 +23,6 @@ function xmldb_message_pushsafer_install() {
     $DB->insert_record('message_processors', $provider);
     return false;
 }
+
+
 ?>
--- db/upgrade.php
+++ db/upgrade.php
@@ -5,7 +5,7 @@
  * Pushsafer for Moodle is distributed as GPLv3 software, and is provided free of charge without warranty
  * A full copy of this licence can be found @ http://www.gnu.org/licenses/gpl.html
  *
- * @package moodle-message_pushsafer
+ * @package message_pushsafer
  * @author Kevin Siml https://www.pushsafer.com
  * @copyright Copyright 2017 Kevin Siml pushsafer.com.
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public Licence v3 (See code header for additional terms)
